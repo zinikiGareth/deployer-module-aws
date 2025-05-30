84ea43d1a948bac051381509bbe0114b1b19353e
Gareth Powell <gareth@ziniki.org>
2025-05-30 07:54:08 +0100

    PourInto has contents

--- internal/s3/bucket_transfer.go
+++ internal/s3/bucket_transfer.go
@@ -2,8 +2,8 @@ package s3
 
 import (
 	"context"
+	"io"
 	"log"
-	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
@@ -14,13 +14,12 @@ type bucketTransfer struct {
 	bucket string
 }
 
-func (b *bucketTransfer) PourInto(key string) {
+func (b *bucketTransfer) PourInto(key string, contents io.Reader) {
 	log.Printf("want to pour %s into %s", key, b.bucket)
-	body := strings.NewReader("hello, world")
 	b.client.PutObject(context.TODO(), &s3.PutObjectInput{
 		Bucket: aws.String(b.bucket),
 		Key:    aws.String(key),
-		Body:   body,
+		Body:   contents,
 	})
 }
 
