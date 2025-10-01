1a1581957836cfd26cbb2a63d1269c79a041e3ba
Gareth Powell <gareth@ziniki.org>
2025-10-01 08:49:12 +0100

    Finished up first cut of creating aurora dsql cluster

--- internal/auroraDSQL/cluster_creator.go
+++ internal/auroraDSQL/cluster_creator.go
@@ -21,19 +21,6 @@ type ClusterCreator struct {
 	Client *dsql.Client
 }
 
-/*
-	func (cc *ClusterCreator) ShortDescription() string {
-		return "aws.Neptune.Cluster[" + cc.name + "]"
-	}
-
-	func (cc *ClusterCreator) DumpTo(iw driverbottom.IndentWriter) {
-		iw.Intro("aws.Neptune.Cluster[")
-		iw.AttrsWhere(cc)
-		iw.TextAttr("named", cc.name)
-		iw.EndAttrs()
-	}
-*/
-
 func (cc *ClusterCreator) DetermineInitialState(creator *corepkg.CoreCreator, pres corebottom.ValuePresenter) {
 	creator.GetEnv("aws.AwsEnv", reflect.TypeFor[*env.AwsEnv](), "AuroraClient", "Client")
 	log.Printf("client = %T %p\n", cc.Client, cc.Client)
@@ -48,48 +35,12 @@ func (cc *ClusterCreator) DetermineInitialState(creator *corepkg.CoreCreator, pr
 }
 
 func (cc *ClusterCreator) DetermineDesiredState(creator *corepkg.CoreCreator, pres corebottom.ValuePresenter) {
-	model := &clusterModel{id: creator.Name()}
-	/*
-		for k, p := range cc.props {
-			v := cc.tools.Storage.Eval(p)
-			switch k.Id() {
-			case "SubnetGroupName":
-				subnetGroup, ok := v.(*subnetModel)
-				if !ok {
-					log.Fatalf("SubnetGroupName did not point to a subnet model")
-				}
-				model.subnetGroup = subnetGroup.name
-			case "MinCapacity":
-				cap, ok := v.(float64)
-				if ok {
-					model.minCapacity = cap
-				}
-			case "MaxCapacity":
-				cap, ok := v.(float64)
-				if ok {
-					model.maxCapacity = cap
-				}
-			default:
-				log.Fatalf("neptune cluster does not support a parameter %s\n", k.Id())
-			}
-		}
-		if model.subnetGroup == "" {
-			cc.tools.Reporter.ReportAtf(cc.loc, "SubnetGroupName required for Neptune Cluster")
-			return
-		}
-		if model.maxCapacity != 0 && model.minCapacity != 0 {
-			// that's fine
-		} else if model.maxCapacity != 0 || model.minCapacity != 0 {
-			cc.tools.Reporter.ReportAtf(cc.loc, "must either specify neither MinCapacity or MaxCapacity or both")
-			return
-		}
-	*/
-	log.Printf("have desired aurora config for %s\n", model.id)
+	model := &clusterModel{}
+	log.Printf("have desired aurora config for %s\n", creator.Name())
 	pres.Present(model)
 }
 
 func (cc *ClusterCreator) UpdateReality(creator *corepkg.CoreCreator, initial any, desired any) {
-
 	if initial != nil {
 		found := initial.(*clusterAWSModel)
 		log.Printf("cluster %s already existed for %s\n", found.arn, creator.Name())
@@ -101,14 +52,6 @@ func (cc *ClusterCreator) UpdateReality(creator *corepkg.CoreCreator, initial an
 	tags["Name"] = creator.Name()
 	ci := &dsql.CreateClusterInput{Tags: tags}
 
-	/*
-		minCap := 1.0
-		maxCap := 1.0
-		if desired.minCapacity != 0 && desired.maxCapacity != 0 {
-			scaling := &types.ServerlessV2ScalingConfiguration{MinCapacity: &minCap, MaxCapacity: &maxCap}
-			ci.ServerlessV2ScalingConfiguration = scaling
-		}
-	*/
 	create, err := cc.Client.CreateCluster(context.TODO(), ci)
 	if err != nil {
 		log.Fatalf("failed to create cluster %s: %v\n", creator.Name(), err)
@@ -124,39 +67,34 @@ func (cc *ClusterCreator) UpdateReality(creator *corepkg.CoreCreator, initial an
 	creator.Created(created)
 }
 
-func (cc *ClusterCreator) TearDown(creator *corepkg.CoreCreator, initial any) {
-	/*
-		tmp := cc.tools.Storage.GetCoin(cc.coin, corebottom.DETERMINE_INITIAL_MODE)
+func (cc *ClusterCreator) TearDown(creator *corepkg.CoreCreator, initial any, teardown corebottom.TearDown) {
+	if initial == nil {
+		log.Printf("no cluster existed for %s\n", creator.Name())
+		return
+	}
 
-		if tmp == nil {
-			log.Printf("no cluster existed for %s\n", cc.name)
-			return
-		}
+	found := initial.(*clusterAWSModel)
+	log.Printf("you have asked to tear down aurora dsql cluster for %s (arn: %s) with mode %s\n", creator.Name(), found.arn, teardown.Mode())
+	// todo: allow @teardown finalSnapshot
+	// will require @finalShapshotIdentifier
+	switch teardown.Mode() {
+	case "preserve":
+		log.Printf("not deleting cluster %s because teardown mode is 'preserve'", creator.Name())
+	case "delete":
+		log.Printf("deleting cluster for %s with teardown mode 'delete'", creator.Name())
+		cc.deleteCluster(creator, found)
+	default:
+		log.Printf("cannot handle teardown mode '%s' for bucket %s", teardown.Mode(), creator.Name())
+	}
 
-		found := tmp.(*clusterModel)
-		log.Printf("you have asked to tear down neptune cluster for %s (arn: %s) with mode %s\n", found.name, found.arn, cc.teardown.Mode())
-		// todo: allow @teardown finalSnapshot
-		// will require @finalShapshotIdentifier
-		switch cc.teardown.Mode() {
-		case "preserve":
-			log.Printf("not deleting cluster %s because teardown mode is 'preserve'", found.name)
-		case "delete":
-			log.Printf("deleting cluster for %s with teardown mode 'delete'", found.name)
-			cc.deleteCluster(found, "")
-		default:
-			log.Printf("cannot handle teardown mode '%s' for bucket %s", cc.teardown.Mode(), found.name)
-		}
+	utils.ExponentialBackoff(func() bool {
+		return cc.waitForDeletion(creator, found)
+	})
 
-		utils.ExponentialBackoff(func() bool {
-			return cc.waitForDeletion(found)
-		})
-
-		log.Printf("deleted neptune cluster %s", found.name)
-	*/
+	log.Printf("deleted neptune cluster %s", creator.Name())
 }
 
 type clusterModel struct {
-	id string
 }
 
 type clusterAWSModel struct {
@@ -168,8 +106,8 @@ func (cc *ClusterCreator) findClusterNamed(name string) *clusterAWSModel {
 	var tok *string
 	for {
 		clusters, err := cc.Client.ListClusters(context.TODO(), &dsql.ListClustersInput{NextToken: tok})
-		if !clusterExists(err) {
-			return nil
+		if err != nil {
+			panic(err)
 		}
 		for _, c := range clusters.Clusters {
 			tags, err := cc.Client.ListTagsForResource(context.TODO(), &dsql.ListTagsForResourceInput{ResourceArn: c.Arn})
@@ -195,7 +133,7 @@ func clusterExists(err error) bool {
 	if ok {
 		e2, ok := e1.Err.(*ht.ResponseError)
 		if ok {
-			if e2.ResponseError.Response.StatusCode == 400 {
+			if e2.ResponseError.Response.StatusCode == 404 {
 				return false
 			}
 			log.Fatalf("error: %v %v", e2.ResponseError.Response.StatusCode, e2.Response.Status)
@@ -205,22 +143,6 @@ func clusterExists(err error) bool {
 	log.Fatalf("getting clusters failed: %T %v", err, err)
 	panic("failed")
 }
-
-/*
-func (cc *ClusterCreator) deleteCluster(cluster *clusterModel, finalSnapshotId string) {
-	args := &neptune.DeleteDBClusterInput{DBClusterIdentifier: &cluster.name}
-	if finalSnapshotId != "" {
-		args.FinalDBSnapshotIdentifier = &finalSnapshotId
-	} else {
-		skip := true
-		args.SkipFinalSnapshot = &skip
-	}
-	_, err := cc.client.DeleteDBCluster(context.TODO(), args)
-	if err != nil {
-		log.Fatalf("deleting cluster failed: %T %v", err, err)
-	}
-}
-*/
 
 func (cc *ClusterCreator) waitForCreation(cluster *clusterAWSModel) bool {
 	c, err := cc.Client.GetCluster(context.TODO(), &dsql.GetClusterInput{Identifier: &cluster.id})
@@ -236,29 +158,32 @@ func (cc *ClusterCreator) waitForCreation(cluster *clusterAWSModel) bool {
 	return false
 }
 
-/*
-func (cc *ClusterCreator) waitForDeletion(cluster *clusterModel) bool {
-	clusters, err := cc.client.DescribeDBClusters(context.TODO(), &neptune.DescribeDBClustersInput{DBClusterIdentifier: &cluster.name})
-	if !clusterExists(err) || len(clusters.DBClusters) == 0 {
+func (cc *ClusterCreator) deleteCluster(creator *corepkg.CoreCreator, cluster *clusterAWSModel) {
+	f := false
+	mod := &dsql.UpdateClusterInput{Identifier: &cluster.id, DeletionProtectionEnabled: &f}
+	_, err := cc.Client.UpdateCluster(context.TODO(), mod)
+	if err != nil {
+		log.Fatalf("failed to remove deletion protection from cluster %s: %v", creator.Name(), err)
+	}
+
+	args := &dsql.DeleteClusterInput{Identifier: &cluster.id}
+	_, err = cc.Client.DeleteCluster(context.TODO(), args)
+	if err != nil {
+		log.Fatalf("deleting cluster failed: %T %v", err, err)
+	}
+}
+
+func (cc *ClusterCreator) waitForDeletion(creator *corepkg.CoreCreator, cluster *clusterAWSModel) bool {
+	c, err := cc.Client.GetCluster(context.TODO(), &dsql.GetClusterInput{Identifier: &cluster.id})
+	if !clusterExists(err) || c.Status == types.ClusterStatusDeleted {
 		return true
 	}
-	c := clusters.DBClusters[0]
-	if c.Status != nil {
-		if *c.Status == "deleting" || *c.Status == "available" {
-			log.Printf("cluster %s still exists with status %s\n", cluster.name, *c.Status)
-		} else {
-			log.Fatalf("status was %s, not available or deleting", *c.Status)
-		}
+	if c.Status == types.ClusterStatusDeleting || c.Status == types.ClusterStatusActive {
+		log.Printf("cluster %s still exists with status %v\n", creator.Name(), c.Status)
 	} else {
-		log.Printf("cluster %s still exists with nil status\n", cluster.name)
+		log.Fatalf("status was %v, not available or deleting", c.Status)
 	}
 	return false
 }
-
-func (cc *ClusterCreator) String() string {
-	return fmt.Sprintf("ClusterCreator[%s]", cc.name)
-}
-
-*/
 
 var _ corepkg.CreationStrategy = &ClusterCreator{}
